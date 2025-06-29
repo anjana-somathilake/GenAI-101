b88bacc85b151d320db781a4dca93350b4a4bb76
Anjana(AJ) Somathilake <anjana.somathilake@dbizsolution.com>
2025-06-29 22:13:22 +1000

    models

--- Lessons/02 - Models/01 text_in text_out model.py
+++ Lessons/02 - Models/01 text_in text_out model.py
@@ -2,16 +2,14 @@
 from langchain_core.prompts import PromptTemplate
 from langchain_ollama import OllamaLLM
 
-llm = OllamaLLM(model="llama3.2-vision")
+llm = OllamaLLM(model="qwen2:0.5b")
 
 template = PromptTemplate.from_template(
-    "What is in this {image}"
+    "Generate a detailed prompt as a expert in fantasy story writing for an LLM to this given {basic_prompt} and limit to 100 words"
 )
 
 
-prompt = template.format(image="image.jp")
-
+prompt = template.format(basic_prompt="write a poem about a dog")
 
 response = llm.invoke(prompt)
 print(response)
-
