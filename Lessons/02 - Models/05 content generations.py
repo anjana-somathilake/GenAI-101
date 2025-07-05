21c0d0cf2d49b70581b84afd722f5b1f3fefd79c
Anjana(AJ) Somathilake <anjana.somathilake@dbizsolution.com>
2025-07-06 09:33:17 +1000

    model for the job

---
+++ Lessons/02 - Models/05 content generations.py
@@ -0,0 +1,20 @@
+
+
+from langchain_core.prompts import PromptTemplate
+from langchain_ollama import OllamaLLM
+
+# llm = OllamaLLM(model="llama3.1:latest")
+# llm = OllamaLLM(model="gemma2:2b")
+# llm = OllamaLLM(model="gemma3:1b")
+# llm = OllamaLLM(model="gemma2:latest")
+llm = OllamaLLM(model="qwen2:0.5b")
+
+template = PromptTemplate.from_template("generate 5 quotes based on the {keywords}")
+
+words = input('Give me something to work on>')
+
+
+prompt = template.format(keywords=words)
+
+response = llm.invoke(prompt)
+print(response)
