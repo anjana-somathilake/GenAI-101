b15698ce1f6bc377440163b7f422994b00311aac
Anjana(AJ) Somathilake <anjana.somathilake@dbizsolution.com>
2025-09-16 20:43:07 +1000

    better auto complete example

--- Lessons/01 - Prompts/01  auto complete.py
+++ Lessons/01 - Prompts/01  auto complete.py
@@ -3,7 +3,7 @@ from langchain_ollama import OllamaLLM
 
 llm = OllamaLLM(model="qwen2:0.5b")
 
-response = llm.invoke("Brown fox jumps ...")
-
+# response = llm.invoke("Brown fox jumps ...")
+response = llm.invoke("Complete the follwoing: Once a upon a time there was a ")
 print(response)
 
